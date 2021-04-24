6624cd37b9a9fe3cb7040ec6f9f3a82464832312
15872427706 <1063889783@qq.com>
2021-04-24 10:42:03 +0800

    购物车8989

--- gulimallproduct/src/main/java/com/atguigu/gulimall/pms/controller/AttrController.java
+++ gulimallproduct/src/main/java/com/atguigu/gulimall/pms/controller/AttrController.java
@@ -30,7 +30,6 @@ public class AttrController {
     @GetMapping("/base/list/{catelogId}")
     public R baseAttrList(@RequestParam Map<String, Object> params,@PathVariable("catelogId") Long catelogId){
         PageUtils page = attrService.queryBasePage(params,catelogId);
-        System.out.println("2222222");
         return R.ok().put("page",page);
     }
 
